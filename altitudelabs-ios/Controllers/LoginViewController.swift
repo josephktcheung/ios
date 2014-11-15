72b53a71c89cb22835715c039bf7c8116f19923a
josephktcheung <josephktcheung@gmail.com>
2014-11-15 08:58:18 +0800

    Set up login view and controller

---
+++ altitudelabs-ios/Controllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  altitudelabs-ios
+//
+//  Created by Joseph Cheung on 15/11/14.
+//  Copyright (c) 2014 josephktcheung. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+}
